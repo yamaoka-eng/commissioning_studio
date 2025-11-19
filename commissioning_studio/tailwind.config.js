e3640dd1270ca0e675e840f6a5b71b1b4d7534bc
yamaoka-eng <554209835@qq.com>
2025-11-20 01:56:49 +0800

    首次提交

---
+++ commissioning_studio/tailwind.config.js
@@ -0,0 +1,8 @@
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['**/*.razor', '**/*.cshtml', '**/*.html', '**/*.cs', '**/*.js'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
